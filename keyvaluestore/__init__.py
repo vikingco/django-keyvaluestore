915094731e274389241c58d53ab905832fcd0262
Niels Van Och <niels.vanoch@gmail.com>
2015-07-02 14:59:09 +0200

    Clean up git versioning

--- keyvaluestore/__init__.py
+++ keyvaluestore/__init__.py
@@ -1,23 +1,14 @@
-import datetime
-import os
-import subprocess
+from datetime import datetime
+from os.path import abspath, dirname, join
+from subprocess import check_output
 
 def get_git_version():
-    git_dir = os.path.abspath(
-        os.path.join(
-            os.path.abspath(os.path.dirname(__file__)),
-            '..',
-            '.git'
-        )
-    )
+    git_dir = abspath(join(abspath(dirname(__file__)), '..', '.git'))
+
     try:
-        # Python 2.7 has subprocess.check_output
-        # 2.6 needs this longer version
-        git_info = subprocess.Popen(['git', '--git-dir=%s' % git_dir, 'log', '--pretty=%ct %h', '-1'], stdout=subprocess.PIPE).communicate()[0].split()
-        git_time = datetime.datetime.fromtimestamp(float(git_info[0]))
-    except Exception:
-        git_time = datetime.datetime.now()
-        git_info = ('', '0000000')
-    return git_time.strftime('%Y.%m.%d') + '.' + git_info[1]
+        git_info = check_output('git --git-dir="%s" log --pretty="%%ct %%h" -1' % git_dir, shell=True).split()
+        return '%s.%s' % (datetime.fromtimestamp(float(git_info[0])).strftime('%Y.%m.%d'), git_info[1])
+    except:
+        return '%s.0000000' % datetime.now()
 
 __version__ = get_git_version()
